20b971eec4671390c1c98c654e43cca9185fcac7
Anton Chumakov <anton@chumakoff.com>
2024-10-01 23:36:47 +0300

    HW0: Implementing Optional 2

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -8,8 +8,14 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
 import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.toList;
 import static ru.javawebinar.topjava.util.TimeUtil.isBetweenHalfOpen;
 
 public class UserMealsUtil {
@@ -28,6 +34,7 @@ public class UserMealsUtil {
         mealsTo.forEach(System.out::println);
 
         System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
+        System.out.println(filteredByStreamsOptional2(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
     }
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
@@ -55,11 +62,88 @@ public class UserMealsUtil {
                 .collect(Collectors.toList());
     }
 
+    public static List<UserMealWithExcess> filteredByStreamsOptional2(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+        return meals.stream().collect(
+                Collectors.groupingBy(
+                        meal -> dayIndex(meal),
+                        new DailyMealsAggregationCollector(startTime, endTime, caloriesPerDay)
+                )
+        ).values().stream().flatMap(value -> value.getMealsWithExcess().stream()).collect(Collectors.toList());
+    }
+
     private static UserMealWithExcess toUserMealWithExcess(UserMeal meal, boolean excess) {
         return new UserMealWithExcess(meal.getDateTime(), meal.getDescription(), meal.getCalories(), excess);
     }
 
     private static LocalDate dayIndex(UserMeal meal) {
         return meal.getDateTime().toLocalDate();
+    }
+
+    public static class DailyMealsAggregation {
+        private LocalTime startTime;
+        private LocalTime endTime;
+        private List<UserMeal> mealsInTimeRange = new ArrayList<>();
+        private int totalCalories = 0;
+        private int maxCalories = 0;
+
+        DailyMealsAggregation(LocalTime startTime, LocalTime endTime, int maxCalories) {
+            this.startTime = startTime;
+            this.endTime = endTime;
+            this.maxCalories = maxCalories;
+        }
+
+        public void takeMealIntoAccount(UserMeal meal) {
+            totalCalories += meal.getCalories();
+            if (isBetweenHalfOpen(meal.getDateTime().toLocalTime(), startTime, endTime)) mealsInTimeRange.add(meal);
+        }
+
+        public List<UserMealWithExcess> getMealsWithExcess() {
+            return mealsInTimeRange.stream().map(meal -> toUserMealWithExcess(meal, isExcess())).collect(toList());
+        }
+
+        private Boolean isExcess() {
+            return totalCalories > maxCalories;
+        }
+    }
+
+    public static class DailyMealsAggregationCollector implements Collector<UserMeal, DailyMealsAggregation, DailyMealsAggregation> {
+        private LocalTime startTime;
+        private LocalTime endTime;
+        private int maxCalories;
+
+        DailyMealsAggregationCollector(LocalTime startTime, LocalTime endTime, int maxCalories) {
+            this.startTime = startTime;
+            this.endTime = endTime;
+            this.maxCalories = maxCalories;
+        }
+
+        @Override
+        public Supplier<DailyMealsAggregation> supplier() {
+            return () -> new DailyMealsAggregation(startTime, endTime, maxCalories);
+        }
+
+        @Override
+        public BiConsumer<DailyMealsAggregation, UserMeal> accumulator() {
+            return DailyMealsAggregation::takeMealIntoAccount;
+        }
+
+        @Override
+        public BinaryOperator<DailyMealsAggregation> combiner() {
+            return (aggr1, aggr2) -> {
+                aggr1.mealsInTimeRange.addAll(aggr2.mealsInTimeRange);
+                aggr1.totalCalories += aggr2.totalCalories;
+                return aggr1;
+            };
+        }
+
+        @Override
+        public Function<DailyMealsAggregation, DailyMealsAggregation> finisher() {
+            return aggr -> aggr;
+        }
+
+        @Override
+        public Set<Characteristics> characteristics() {
+            return Collections.emptySet();
+        }
     }
 }
--- src/test/java/ru/javawebinar/topjava/util/UserMealsUtilTest.java
+++ src/test/java/ru/javawebinar/topjava/util/UserMealsUtilTest.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static ru.javawebinar.topjava.util.UserMealsUtil.filteredByCycles;
+import static ru.javawebinar.topjava.util.UserMealsUtil.filteredByStreamsOptional2;
 
 class UserMealsUtilTest {
     private static List<UserMeal> meals;
@@ -29,7 +30,7 @@ class UserMealsUtilTest {
                 new UserMeal(LocalDateTime.parse("2020-01-31T13:00"), "2020-01-31_13", 400),
                 new UserMeal(LocalDateTime.parse("2020-01-31T20:00"), "2020-01-31_20", 401),
                 new UserMeal(LocalDateTime.parse("2020-03-30T10:00"), "2020-03-30_10", 9999),
-                new UserMeal(LocalDateTime.parse("2021-01-30T10:00"), "2021-01-30_10", 9999)
+                new UserMeal(LocalDateTime.parse("2021-01-30T10:00"), "2021-01-30_10", 2000)
         );
     }
 
@@ -51,7 +52,7 @@ class UserMealsUtilTest {
         assertTrue(mealsWithExcess.get(3).isExcess());
 
         assertEquals(mealsWithExcess.get(4).getDescription(), "2021-01-30_10");
-        assertTrue(mealsWithExcess.get(4).isExcess());
+        assertFalse(mealsWithExcess.get(4).isExcess());
     }
 
     @Test
@@ -72,6 +73,29 @@ class UserMealsUtilTest {
         assertTrue(mealsWithExcess.get(3).isExcess());
 
         assertEquals(mealsWithExcess.get(4).getDescription(), "2021-01-30_10");
-        assertTrue(mealsWithExcess.get(4).isExcess());
+        assertFalse(mealsWithExcess.get(4).isExcess());
+    }
+
+    @Test
+    void filteredByStreamsOptional2Test() {
+        List<UserMealWithExcess> mealsWithExcess = filteredByStreamsOptional2(meals, LocalTime.parse("07:00"), LocalTime.parse("12:00"), 2000);
+        assertEquals(mealsWithExcess.size(), 5);
+
+        UserMealWithExcess meal;
+
+        meal = mealsWithExcess.stream().filter(m -> m.getDescription().equals("2020-01-30_10")).findFirst().get();
+        assertFalse(meal.isExcess());
+
+        meal = mealsWithExcess.stream().filter(m -> m.getDescription().equals("2020-01-31_07")).findFirst().get();
+        assertTrue(meal.isExcess());
+
+        meal = mealsWithExcess.stream().filter(m -> m.getDescription().equals("2020-01-31_10")).findFirst().get();
+        assertTrue(meal.isExcess());
+
+        meal = mealsWithExcess.stream().filter(m -> m.getDescription().equals("2020-03-30_10")).findFirst().get();
+        assertTrue(meal.isExcess());
+
+        meal = mealsWithExcess.stream().filter(m -> m.getDescription().equals("2021-01-30_10")).findFirst().get();
+        assertFalse(meal.isExcess());
     }
 }
